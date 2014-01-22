a54492f6133a11657ac6621eb30255fe3cd879b7
Alex Notov <anotov@gmail.com>
2014-01-22 15:51:28 -0800

    Filtering by domain

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,15 @@
 class PeopleController < ApplicationController
   def index
+    # remember the domain name selected
+    @email_domain = params[:email_domain] || 'All'
+
+    # get all people
     @people = Person.all
+
+    # get all the available domain names
     @email_domains = @people.map { |p| p.email.split('@')[1] }.uniq.sort
+
+    # filter the people based on domain name, when provided
+    @people.to_a.select! { |p| p.email.index @email_domain } unless @email_domain == 'All'
   end
 end
