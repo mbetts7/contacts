a29dffd741ad045af847c533abc9b9759874ed38
Alex Notov <anotov@gmail.com>
2014-01-22 16:33:52 -0800

    Quick route formatting fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Contacts::Application.routes.draw do
   root to: 'people#index' 
   get 'people', to: 'people#index'
-  get 'people/with_domain/:email_domain', to: 'people#index', constraints: { email_domain: /[^\/]+/ }
+  get 'people/with_domain/:email_domain', 
+    to: 'people#index', 
+    constraints: { email_domain: /[^\/]+/ }
 end
