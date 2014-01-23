b52d266e336cb08672c43b77199f9c44297b7c89
Alex Notov <anotov@gmail.com>
2014-01-23 08:49:54 -0800

    Updates to spec and README

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Person do
   describe :find_all_with_email_domain do
-    context 'given a domain name' do
-      before do
-        # create 3 people, only one of which has the yahoo.com domain name
-        @john = Person.create({ first_name: 'John', last_name: 'Doe', email: 'john.doe@gmail.com' })
-        @jane = Person.create({ first_name: 'Jane', last_name: 'Doe', email: 'jane.doe@yahoo.com' })
-        @mary = Person.create({ first_name: 'Mary', last_name: 'Jane', email: 'mary.jane@gmail.com' })
-      end
+    before do
+      # create 3 people, only one of which has the yahoo.com domain name
+      @john = Person.create({ first_name: 'John', last_name: 'Doe', email: 'john.doe@gmail.com', domain_name: 'gmail.com' })
+      @jane = Person.create({ first_name: 'Jane', last_name: 'Doe', email: 'jane.doe@yahoo.com', domain_name: 'yahoo.com' })
+      @mary = Person.create({ first_name: 'Mary', last_name: 'Jane', email: 'mary.jane@gmail.com', domain_name: 'gmail.com' })
+    end
 
+    context 'given a domain name' do
       it 'should return an active record relation' do
         Person.find_all_with_email_domain('yahoo.com').should be_an ActiveRecord::Relation
       end
@@ -19,7 +19,13 @@ describe Person do
       end
     end
 
-    context 'given no domain name' do
+    context 'given \'All\'' do
+      it 'should return all people' do
+        Person.find_all_with_email_domain('All').should == [@john, @jane, @mary]
+      end
+    end
+
+    context 'given nil' do
       it 'should return no people' do
         Person.find_all_with_email_domain.should == []
       end
@@ -29,12 +35,12 @@ describe Person do
   describe :all_email_domains do
     context 'when there are people in the database' do
       before do
-        @john = Person.create({ first_name: 'John', last_name: 'Doe', email: 'john.doe@gmail.com' })
-        @jane = Person.create({ first_name: 'Jane', last_name: 'Doe', email: 'jane.doe@yahoo.com' })
+        @john = Person.create({ first_name: 'John', last_name: 'Doe', email: 'john.doe@gmail.com', domain_name: 'gmail.com' })
+        @jane = Person.create({ first_name: 'Jane', last_name: 'Doe', email: 'jane.doe@yahoo.com', domain_name: 'yahoo.com' })
       end
 
       it 'should return the domains for those people' do
-        Person.all_email_domains.should == ['yahoo.com', 'gmail.com']
+        Person.all_email_domains.should =~ ['yahoo.com', 'gmail.com']
       end
     end 
 
